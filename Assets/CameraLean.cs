a9a58b112213fb9c4843d072633989619784b4af
Sean Rice <seane.rice@gmail.com>
2018-10-22 23:43:18 -0400

    Initial Commit

---
+++ Assets/CameraLean.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLean : MonoBehaviour {
+	public float RotAmt = 5;
+
+	public Vector3 StartRot;
+	
+	private float TimeX = 0, TimeY = 0, TimeZ = 0;
+	private bool countX = false, countY = false, countZ = false;
+
+	// Use this for initialization
+	void Start () {
+		StartRot = transform.localEulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (countX) {
+			TimeX += Time.deltaTime;
+		} else {
+			TimeX -= Time.deltaTime;
+		}
+
+		if (countY) {
+			TimeY += Time.deltaTime;
+		} else {
+			TimeY -= Time.deltaTime;
+		}
+
+		if (countZ) {
+			TimeZ += Time.deltaTime;
+		} else {
+			TimeZ -= Time.deltaTime;
+		}
+
+		TimeX = Mathf.Clamp01(TimeX);
+		TimeY = Mathf.Clamp01(TimeY);
+		TimeZ = Mathf.Clamp01(TimeZ);
+
+
+	}
+
+	private Vector3 Slerp(Vector3 start, Vector3 end, float t) {
+		float x = Mathf.SmoothStep(start.x, end.x, t);
+		float y = Mathf.SmoothStep(start.y, end.y, t);
+		float z = Mathf.SmoothStep(start.z, end.z, t);
+		return new Vector3(x, y, z);
+	}
+}
---
+++ Assets/GameMain.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMain : MonoBehaviour {
+
+	public float Speed = 5;
+	public float TurnSpeed = 10;
+
+	private Text ScoreText;
+
+	private int Score = 0;
+
+	public Vector3 LowerBounds = new Vector3(-50, -50, -50);
+	public Vector3 UpperBounds = new Vector3(50, 50, 50);
+
+	private bool IsRotating = false;
+
+	public ParticleSystem WarpParticles;
+
+	// Use this for initialization
+	void Start () {
+		SetScore(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 dir = transform.forward;
+		Vector3 right = transform.right;
+		transform.position += dir * Time.deltaTime * Speed;
+		IsRotating = false;
+
+
+		if (Input.GetKey(KeyCode.D)) {
+			transform.eulerAngles += new Vector3(0, Time.deltaTime * Speed, 0);
+			IsRotating = true;
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			transform.eulerAngles -= new Vector3(0, Time.deltaTime * Speed, 0);
+			IsRotating = true;
+		}
+		if (Input.GetKey(KeyCode.W)) {
+			transform.eulerAngles += new Vector3(Time.deltaTime * Speed, 0, 0);
+			IsRotating = true;
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			transform.eulerAngles -= new Vector3(Time.deltaTime * Speed, 0, 0);
+			IsRotating = true;
+		}
+
+		float x = transform.position.x;
+		float y = transform.position.y;
+		float z = transform.position.z;
+		if (x > UpperBounds.x) x = LowerBounds.x;
+		if (x < LowerBounds.x) x = UpperBounds.x;
+		if (y > UpperBounds.y) y = LowerBounds.y;
+		if (y < LowerBounds.y) y = UpperBounds.y;
+		if (z > UpperBounds.z) z = LowerBounds.z;
+		if (z < LowerBounds.z) z = UpperBounds.z;
+		transform.position = new Vector3(x, y, z);
+
+
+		if (IsRotating = true) {
+
+		}
+	}
+
+	private void SetScore(int i) {
+		Score = i;
+		ScoreText.text = "Score: " + Score;
+		// Send OSC message to PD
+	}
+
+	private void AddScore() {
+		SetScore(Score + 1);
+		switch (Score){
+			default:
+				break;
+			case 1:
+				break;
+			case 2:
+				break;
+		}
+	}
+
+	private void OnCollisionEnter (Collision collision) {
+		AddScore();
+	}
+}
